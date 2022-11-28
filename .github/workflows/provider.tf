ac69294a3e475ca7598dea14a4572347de840978
azhar <87310169+azharism@users.noreply.github.com>
2022-11-29 02:35:26 +0530

    Create provider.tf

---
+++ .github/workflows/provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+# Configure the AWS Provider
+
+provider "aws" {
+  version = "~> 4.0"
+  region  = "ap-southeast-1"
+}
+
+
+
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "AKIAZFLLLDRNWEBDN35L"
+  secret_key = "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"
+}
